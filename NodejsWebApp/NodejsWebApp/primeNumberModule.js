791c69f17d744ecc475d2a3e8606c0da29cafa69
Paul Allain <paul.allain17@gmail.com>
2018-11-22 23:27:38 +0000

    Return lower and higer closest primer numbers, check for number entry, prevent favicon from being sent on first call.

--- NodejsWebApp/NodejsWebApp/primeNumberModule.js
+++ NodejsWebApp/NodejsWebApp/primeNumberModule.js
@@ -1,3 +1,33 @@
 ﻿exports.closest = function (number) {
-    return 'Your number is: ' + number;
+    var lowerPrime, higherPrime;
+    var counter = 1;
+    if (isPrime(number)) {
+        return 'You number : ' + number + ' is a prime number.';
+    }
+    while (!(lowerPrime && higherPrime)) {
+
+        if (!higherPrime) {
+            if (isPrime(number + counter)) {
+                higherPrime = number + counter;
+            }
+        }
+        if (!lowerPrime) {
+            if (isPrime(number - counter)) {
+                lowerPrime = number - counter;
+            }
+        }
+        counter++
+    }
+
+    return 'The lower prime number is: ' + lowerPrime +
+        ' The higher prime number is: ' + higherPrime;
 };
+
+function isPrime(value) {
+    for (var i = 2; i < value; i++) {
+        if (value % i === 0) {
+            return false;
+        }
+    }
+    return value > 1;
+}
--- NodejsWebApp/NodejsWebApp/server.js
+++ NodejsWebApp/NodejsWebApp/server.js
@@ -3,11 +3,18 @@ var primeNumber = require('./primeNumberModule');
 var port = process.env.PORT || 1337;
 
 function respond(req, res, next) {
-    res.send(primeNumber.closest(req.params.number));
-    next();
+    var entry = req.params.number;
+    if (!entry || isNaN(parseFloat(entry))) {
+        res.send('Please give a number.');
+        next();
+    } else {
+        res.send(primeNumber.closest(parseFloat(entry)));
+        next();
+    }
 }
 
 var server = restify.createServer();
 server.get('/:number', respond);
+server.get('/favicon.ico', (req, res) => res.status(204));
 
 server.listen(port);
