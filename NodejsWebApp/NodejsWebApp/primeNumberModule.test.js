a92123a0ba2d21515b416162067b8cac12cee63d
Paul Allain <paul.allain17@gmail.com>
2018-11-23 23:23:32 +0000

    New unit test in primeNumberModule

--- NodejsWebApp/NodejsWebApp/primeNumberModule.test.js
+++ NodejsWebApp/NodejsWebApp/primeNumberModule.test.js
@@ -11,4 +11,8 @@ describe('The prime number module', function () {
         assert.equal(primeNumber.closest(13), 'Your number : 13 is a prime number.');
     });
 
+    it('gives the lowest and highest prime number of the given number', function () {
+        assert.equal(primeNumber.closest(12), 'The lower prime number is: 11 The higher prime number is: 13');
+    });
+
 });
