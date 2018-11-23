15917b1c3716c4255162014b51afb1db59a713c9
Paul Allain <paul.allain17@gmail.com>
2018-11-23 23:05:13 +0000

    New unit test.

--- NodejsWebApp/NodejsWebApp/primeNumberModule.test.js
+++ NodejsWebApp/NodejsWebApp/primeNumberModule.test.js
@@ -2,7 +2,13 @@
 const primeNumber = require('./primeNumberModule');
 
 describe('The prime number module', function () {
-    it('gives the given number if it is a prime number', function () {
-        assert.equal(primeNumber.closest(13), 'You number : 13 is a prime number.');
+
+    it('checks if number is greater or equal to 2', function () {
+        assert.equal(primeNumber.closest(1), 'Your number must be greater or equal to 2.');
     });
+
+    it('gives the given number if it is a prime number', function () {
+        assert.equal(primeNumber.closest(13), 'Your number : 13 is a prime number.');
+    });
+
 });
