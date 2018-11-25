57b212aa77b70de631b89e2ac142fab87be0b41d
Paul Allain <paul.allain17@gmail.com>
2018-11-25 13:15:12 +0000

    New requestHandler.test.js file

---
+++ NodejsWebApp/NodejsWebApp/requestHandler.test.js
@@ -0,0 +1,18 @@
+﻿const assert = require('assert');
+const requestHandler = require('./requestHandler');
+const sinon = require('sinon');
+
+describe('The request handler module', function () {
+    it('asks for number if the user did not pass any', function () {
+        const resWithSpy = {
+            send: sinon.spy()
+        };
+        const req = {
+            params: {
+                number: ""
+            }
+        };
+        requestHandler.process(req, resWithSpy);
+        assert.ok(resWithSpy.send.calledWith('Please give a number.'));
+    });
+});
