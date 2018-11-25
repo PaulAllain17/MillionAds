2c1284327434178151fb815449c3bc49d33c1fcf
Paul Allain <paul.allain17@gmail.com>
2018-11-25 23:09:54 +0000

    Scripts to run unit tests using npm, logs to know which port the server is listening to.

--- NodejsWebApp/NodejsWebApp/server.js
+++ NodejsWebApp/NodejsWebApp/server.js
@@ -1,6 +1,6 @@
 const restify = require('restify');
 const requestHandler = require('./requestHandler');
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 8080;
 
 respond = (req, res, next) => {
     requestHandler.process(req, res);
@@ -12,3 +12,6 @@ server.get('/:number', respond);
 server.get('/favicon.ico', (req, res) => res.status(204));
 
 server.listen(port);
+console.log('The server is listening to port ' + port);
+console.log('Use the following url to communicate with the server: http://localhost:' + port + '/');
+console.log('Provide a number after the /');
